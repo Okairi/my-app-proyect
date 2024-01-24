ba04e2e86c4beb6b9264f1ba95b9e7260f4d789d
Alessandro <alessandroRetuerto@hotmail.com>
2024-01-23 22:12:04 -0500

    add param in url get

---
+++ app/api/users/[userId]/route.js
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request, { params }) {
+  const { searchParams } = new URL(request.url);
+  console.log(searchParams);
+
+  console.log(searchParams.get("nam"));
+  console.log(searchParams.get("ape"));
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${params.userId}`
+  );
+
+  const data = await res.json();
+
+  return NextResponse.json({ data });
+}
