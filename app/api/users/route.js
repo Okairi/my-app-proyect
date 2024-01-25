eedcfb90d9e33ee903f0f25a339b9ea735bb0110
Alessandro <alessandroRetuerto@hotmail.com>
2024-01-24 20:28:34 -0500

    add enviroment

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -4,6 +4,8 @@ export async function GET() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
 
+  process.env.TOKEN;
+
   return NextResponse.json({
     message: "OBTENIENDO DATOS",
     data: data,
